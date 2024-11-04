29130696b598220e3f97fcd4d008f346f3546f58
Priyadharshini.S <138849213+priyadharshini225@users.noreply.github.com>
2024-11-04 11:38:54 +0530

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,19 +12,33 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
 #########################################################
 ### your code goes here ###
 
+# Create the SVM classifier
+clf = SVC(kernel='linear')  # or try 'rbf' kernel for improved performance
 
-#########################################################
+# Train the SVM on the training set
+t0 = time()  # start timing
+clf.fit(features_train, labels_train)
+print(f"Training time: {round(time() - t0, 3)} seconds")
+
+# Make predictions on the test set
+t1 = time()  # start timing
+pred = clf.predict(features_test)
+print(f"Prediction time: {round(time() - t1, 3)} seconds")
+
+# Calculate and print the accuracy of the model
+accuracy = accuracy_score(labels_test, pred)
+print(f"Accuracy: {accuracy}")
 
 #########################################################
 '''
@@ -33,7 +47,5 @@ But the Code provided in Quiz has an Indexing issue
 The Code Below solves that issue, So use this one
 '''
 
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
-
-#########################################################
+# Uncomment these lines to use only 1% of the training data for faster training
+# features
